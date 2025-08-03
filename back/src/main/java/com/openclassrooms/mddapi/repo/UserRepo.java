6d49ca257f900284437fe7b20d142749fad2241d
lostmart <martinpedraza1979@gmail.com>
2025-08-03 21:41:42 +0200

    subscriptions created

--- back/src/main/java/com/openclassrooms/mddapi/repo/UserRepo.java
+++ back/src/main/java/com/openclassrooms/mddapi/repo/UserRepo.java
@@ -6,11 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.openclassrooms.mddapi.model.UserModel;
 
-import lombok.NonNull;
-
-public interface UserRepo extends JpaRepository<UserModel, @NonNull Long> {
+public interface UserRepo extends JpaRepository<UserModel, Long> {
     boolean existsByEmail(String email);
 
+    @SuppressWarnings("null")
     boolean existsById(Long id);
 
     Optional<UserModel> findByEmail(String email);
