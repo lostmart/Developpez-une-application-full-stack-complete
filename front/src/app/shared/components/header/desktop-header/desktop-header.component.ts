5aeccd6ac7a82a7ddc6b9f3159dae32cbd93a15e
lostmart <martinpedraza1979@gmail.com>
2025-08-07 19:31:09 +0200

    new header styles

--- front/src/app/shared/components/header/desktop-header/desktop-header.component.ts
+++ front/src/app/shared/components/header/desktop-header/desktop-header.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-desktop-header',
   templateUrl: './desktop-header.component.html',
-  styleUrls: ['./desktop-header.component.scss']
+  styleUrls: ['./desktop-header.component.scss'],
 })
-export class DesktopHeaderComponent implements OnInit {
+export class DesktopHeaderComponent {
+  constructor(
+    private location: Location,
+    public authService: AuthService,
+    private router: Router
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
--- front/src/app/shared/components/header/mobile-header/mobile-header.component.ts
+++ front/src/app/shared/components/header/mobile-header/mobile-header.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-mobile-header',
   templateUrl: './mobile-header.component.html',
   styleUrls: ['./mobile-header.component.scss'],
 })
-export class MobileHeaderComponent implements OnInit {
+export class MobileHeaderComponent {
   showMenu = false;
+
+  constructor(private auth: AuthService, private router: Router) {}
 
   toggleMenu() {
     this.showMenu = !this.showMenu;
   }
 
   logout() {
-    // handle logout
+    this.auth.logout();
+    this.toggleMenu();
+    this.router.navigate(['/login']);
   }
-
-  ngOnInit(): void {}
 }
